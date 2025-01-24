ffb003e077f02ce4d1400137674aa7f095b5976d
SlugFiller <5435495+SlugFiller@users.noreply.github.com>
2025-01-24 14:58:07 +0200

    Mouse movement adjustment

--- scripts/mouse.gd
+++ scripts/mouse.gd
@@ -9,6 +9,8 @@ enum MouseDir {
 @export var direction: MouseDir = MouseDir.RIGHT
 
 const RUN_SPEED := 100.0
+const RUN_SPEED_DOWNSLOPE := 150.0
+const SLOPE_SPEED := 10.0
 const MIN_RUN_SPEED := 1.0
 const GRAVITY := 100.0
 const MAX_FALL := 500.0
@@ -19,12 +21,17 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	var use_run_animation := true
+	var floor_move := false
 	if is_on_floor():
+		var speed := RUN_SPEED
+		if get_real_velocity().y > SLOPE_SPEED:
+			speed = RUN_SPEED_DOWNSLOPE
+		floor_move = true
 		match self.direction:
 			MouseDir.LEFT:
-				self.velocity.x = -RUN_SPEED
+				self.velocity.x = -speed
 			MouseDir.RIGHT:
-				self.velocity.x = RUN_SPEED
+				self.velocity.x = speed
 		self.velocity.y = 0
 	else:
 		self.velocity.y += GRAVITY * delta
@@ -32,11 +39,23 @@ func _physics_process(delta: float) -> void:
 			self.velocity.y = MAX_FALL
 		use_run_animation = false
 	move_and_slide()
-	if use_run_animation && abs(get_real_velocity().x) < MIN_RUN_SPEED:
+	var x_dir := get_real_velocity().x
+	if x_dir < -MIN_RUN_SPEED:
+		self.direction = MouseDir.LEFT
+	elif x_dir > MIN_RUN_SPEED:
+		self.direction = MouseDir.RIGHT
+	else:
 		use_run_animation = false
+		if floor_move:
+			match self.direction:
+				MouseDir.LEFT:
+					self.direction = MouseDir.RIGHT
+				MouseDir.RIGHT:
+					self.direction = MouseDir.LEFT
 	if use_run_animation:
 		if sprite.animation != "run":
 			sprite.play("run")
 	else:
 		if sprite.animation != "idle":
 			sprite.play("idle")
+	sprite.flip_h = self.direction == MouseDir.LEFT
