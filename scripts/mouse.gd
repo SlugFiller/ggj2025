9bb12c9b256ebb3a6aa7ab380f19ce0a7d54457f
SlugFiller <5435495+SlugFiller@users.noreply.github.com>
2025-01-24 20:27:05 +0200

    Death animation

--- scripts/mouse.gd
+++ scripts/mouse.gd
@@ -16,19 +16,22 @@ enum MouseDir {
 var checkpoint: Vector2
 var checkpoint_direction: MouseDir
 var scrollnimation: float = 0.0
+var deathfall: bool = false
 var respawn: bool = false
 var y_shift: float = 0.0
 
-const RUN_SPEED := 100.0
-const RUN_SPEED_DOWNSLOPE := 150.0
+const RUN_SPEED := 200.0
+const RUN_SPEED_DOWNSLOPE := 250.0
 const SLOPE_SPEED := 10.0
 const MIN_RUN_SPEED := 1.0
 const GRAVITY := 100.0
 const FAN_GRAVITY := 300.0
 const MAX_FALL := 500.0
-const DEAD_TIME := 3.0
+const DEAD_TIME := 2.0
 const TUNNEL_TIME := 3.0
 const TIME_SCROLL := 1.0
+const DEATH_FALL_SPEED := 800.0
+const TIME_DEATH_FALL := 1.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -45,12 +48,16 @@ func _physics_process(delta: float) -> void:
 		self.respawn = false
 		self.position = self.checkpoint
 		self.direction = self.checkpoint_direction
+		self.sprite.position = Vector2.ZERO
+		self.deathfall = false
 		self.velocity = Vector2.ZERO
 		self.y_shift = 0.0
 		move_and_slide()
 		return
 	if death_detector.has_overlapping_areas():
+		self.deathfall = true
 		self.scrollnimation = DEAD_TIME
+		sprite.play("death")
 		return
 	for tunnel_candidate in tunnel_detector.get_overlapping_areas():
 		var tunnel := tunnel_candidate as Tunnel
@@ -123,3 +130,5 @@ func _process(delta: float) -> void:
 			if should_poof:
 				broadcast.restart.emit()
 			self.y_shift = (self.checkpoint.y - self.position.y) * (1.0 - self.scrollnimation / TIME_SCROLL)
+		if self.deathfall && self.scrollnimation < TIME_DEATH_FALL:
+			sprite.position.y += DEATH_FALL_SPEED * delta
