3b11ab7125fadd8a16bf7f73ac4f260492f58736
SlugFiller <5435495+SlugFiller@users.noreply.github.com>
2025-01-25 09:50:26 +0200

    Freeze spell

---
+++ scripts/freeze.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var trigger: Area2D = $trigger
+var used: bool = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if used:
+		return
+	for candidate in trigger.get_overlapping_areas():
+		var freezable := candidate as Freezable
+		if freezable == null:
+			continue
+		freezable.frozen.emit()
+		used = true
+	if !used:
+		return
+	var poof := preload("res://items/poof.tscn").instantiate() as Node2D
+	poof.position = self.position
+	get_parent().add_child(poof)
+	queue_free()
