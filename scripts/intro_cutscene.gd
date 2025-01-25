8c594ef097ed5c301413bebb28e8c5f3ed2c3cef
SlugFiller <5435495+SlugFiller@users.noreply.github.com>
2025-01-25 15:22:21 +0200

    Intro after main menu

--- scripts/intro_cutscene.gd
+++ scripts/intro_cutscene.gd
@@ -3,8 +3,8 @@ extends Node2D
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_packed(preload("res://mainmenu.tscn"))
+	get_tree().change_scene_to_packed(preload("res://game.tscn"))
 
 
 func _on_video_stream_player_finished() -> void:
-	get_tree().change_scene_to_packed(preload("res://mainmenu.tscn"))
+	get_tree().change_scene_to_packed(preload("res://game.tscn"))
--- scripts/mainmenu.gd
+++ scripts/mainmenu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_new_game_pressed() -> void:
-	get_tree().change_scene_to_packed(preload("res://game.tscn"))
+	get_tree().change_scene_to_packed(preload("res://intro_cutscene.tscn"))
 
 
 func _on_quit_pressed() -> void:
