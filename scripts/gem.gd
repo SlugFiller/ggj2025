b798bf6e2c9f16a0092907303cab48fb6b39cc89
SlugFiller <5435495+SlugFiller@users.noreply.github.com>
2025-01-26 23:47:53 +0200

    Sparkle on gem pickup

--- scripts/gem.gd
+++ scripts/gem.gd
@@ -21,4 +21,7 @@ func _ready() -> void:
 
 
 func _on_body_entered(_body: Node2D) -> void:
+	var poof := preload("res://items/sparkle.tscn").instantiate() as Node2D
+	poof.position = self.position
+	get_parent().add_child(poof)
 	queue_free()
