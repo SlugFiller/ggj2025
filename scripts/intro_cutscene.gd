9d11dc8679d3ef3fe8564c63347455a6328d5b7a
SlugFiller <5435495+SlugFiller@users.noreply.github.com>
2025-01-27 18:03:15 +0200

    Improvements and captions to intro cutscene

--- scripts/intro_cutscene.gd
+++ scripts/intro_cutscene.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var video_stream_player: VideoStreamPlayer = $VideoStreamPlayer
+var last_position: float = 0.0
 
+func _ready() -> void:
+	ResourceLoader.load_threaded_request("res://game.tscn")
 
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://game.tscn")
+func _process(_delta: float) -> void:
+	var current_position = video_stream_player.stream_position
+	animation_player.advance(current_position - last_position)
+	last_position = current_position
 
-
-func _on_video_stream_player_finished() -> void:
-	get_tree().change_scene_to_file("res://game.tscn")
+func _on_finished() -> void:
+	get_tree().change_scene_to_packed(ResourceLoader.load_threaded_get("res://game.tscn"))
